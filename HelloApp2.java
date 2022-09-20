0d4be0aa5c8f2a05a81c38266b2c865f606acd59
kobepeters1495 <89108261+kobepeters1495@users.noreply.github.com>
2022-09-19 21:54:40 -0400

    streamlined comments in HelloApp2

--- HelloApp2.java
+++ HelloApp2.java
@@ -1,12 +1,12 @@
-/* This application displays a hello message on the console
-by creating an instance of the Greeter class and then
-calling the Greeter object's sayHello method.*/
-
+// This application displays a hello message on
+// the console by creating an instance of the
+// Greeter class and then calling the Greeter
+// object's sayHello method.
 public class HelloApp2                        // HelloApp2 class begins here
 {
   public static void main(String[] args)      // main method is declared
   {
-    Greeter myGreeterObject = new Greeter();  // first line in the body of the main method contains a variable named myGreeterObject
+    Greeter myGreeterObject = new Greeter();  // 1st line in main method body has variable myGreeterObject
     myGreeterObject.sayHello();               // which holds objects created from the Greeter class
   }
 }
